2dbecb22a21cf64ec212b6fbb298aa1efe4fdbb8
diya singh <161337050+ST10440113@users.noreply.github.com>
2026-08-17 18:16:06 +0200

    created both booking pages

---
+++ ReHubCenter_WIL/Controllers/IndividualBookingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReHubCenter_WIL.Controllers
+{
+    public class IndividualBookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
